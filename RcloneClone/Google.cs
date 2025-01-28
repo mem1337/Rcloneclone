dbb746c0762f73f22137c13e173a67a33ea4d13c
aleks <doommilita@gmail.com>
2025-01-28 19:16:28 +0300

    update

---
+++ RcloneClone/Google.cs
@@ -0,0 +1,6 @@
+﻿namespace RcloneClone;
+
+public class Google
+{
+    
+}
---
+++ RcloneClone/Sync.cs
@@ -0,0 +1,42 @@
+﻿namespace RcloneClone;
+
+public class SyncClass
+{
+    public class Sync(Manager manager)
+    {
+        private Manager _manager = manager;
+
+        public async Task<(bool status, string newFolderId)> UploadFile(string fileLocation, string folderID, string mimeType)
+        {
+            var result = await _manager.UploadFile(fileLocation, folderID, mimeType);
+            return (result.status,result.newFolderID);
+        }
+        public async Task<string> GetFolderID(string location, string folderID)
+        {
+            var result = await _manager.GetFolderID(location, folderID);
+            return result;
+        }
+        public async Task<(bool status, string newFolderId)> CompareMetadata(string fileLocation, string folderID)
+        {
+            var result = await _manager.CompareMetaData(fileLocation, folderID);
+            return (result.status, result.newFolderID);
+        }
+        public async Task<string> GetFiles(string folderID)
+        {
+            var result = await _manager.GetFiles(folderID);
+            return result;
+        }
+        public async Task GetBearerToken()
+        {
+            await _manager.GetBearerToken();
+        }
+    }
+}
+public interface Manager
+{
+    public Task<(bool status, string newFolderID)> UploadFile(string fileLocation, string folderID, string mimeType);
+    public Task<string> GetFiles(string folderID);
+    public Task<string> GetFolderID(string location, string folderID);
+    public Task<(bool status, string newFolderID)> CompareMetaData(string fileLocation, string folderID);
+    public Task GetBearerToken();
+}
